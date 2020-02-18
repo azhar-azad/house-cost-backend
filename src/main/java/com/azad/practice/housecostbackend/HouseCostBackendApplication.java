acdce73bf4bb5492e22c2e0750f3a83b586835b2
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-02-18 12:20:30 +0600

    initial commit with db configured

---
+++ src/main/java/com/azad/practice/housecostbackend/HouseCostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.azad.practice.housecostbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCostBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseCostBackendApplication.class, args);
+	}
+
+}
